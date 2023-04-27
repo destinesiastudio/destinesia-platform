80bd250aac4adcda0e325df6bfc9fa4cae2202cf
mayzyo <25147960+mayzyo@users.noreply.github.com>
2023-04-28 08:50:20 +1000

    :alien: Updated api to new redis pattern

--- michael-ai-server/src/history.js
+++ michael-ai-server/src/history.js
@@ -11,7 +11,7 @@ export const chatHistory = async user => {
     let history
     
     try {
-        history = await client.lRange(user, 0, -1)
+        history = await client.lRange(`michael-ai:${user}`, 0, -1)
 
     } catch {
         history = undefined
@@ -26,7 +26,7 @@ export const updateChatHistory = async (user, items) => {
     await client.connect()
 
     for (const item of items) {
-        await client.rPush('michael', JSON.stringify(item))
+        await client.rPush(`michael-ai:${user}`, JSON.stringify(item))
     }
 
     await client.disconnect()
@@ -37,7 +37,7 @@ export const updateChatHistory = async (user, items) => {
 export const clearChatHistory = async user => {
     await client.connect()
     
-    await client.del(user)
+    await client.del(`michael-ai:${user}`)
     await client.disconnect()
 
     return true
